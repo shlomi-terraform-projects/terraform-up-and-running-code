993c145420c1a9f3c2396d2cee0c615e11e2d953
jit-shlomi <shlomi.jit@gmail.com>
2024-04-08 23:12:36 +0300

    fix instance type

--- code/terraform/03-terraform-state/file-layout-example/stage/data-stores/mysql/main.tf
+++ code/terraform/03-terraform-state/file-layout-example/stage/data-stores/mysql/main.tf
@@ -30,7 +30,7 @@ resource "aws_db_instance" "example" {
   identifier_prefix   = "terraform-up-and-running"
   engine              = "mysql"
   allocated_storage   = 10
-  instance_class      = "db.t2.micro"
+  instance_class      = "db.t3.micro"
   skip_final_snapshot = true
 
   db_name             = var.db_name
